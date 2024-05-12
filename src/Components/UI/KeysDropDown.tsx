34fbdc303740dd2c80878647b7726419d48972fe
Chapatta <49091220+Chapatta@users.noreply.github.com>
2024-05-12 10:29:39 +0100

    Added keys dropdown from rest api

--- src/Components/UI/KeysDropDown.tsx
+++ src/Components/UI/KeysDropDown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import * as DataLayer from '../../DataLayer/Test/TestCalls';
-
+import * as useApi from '../../APITool'; 
+import IKey from '../../DataLayer/Interfaces/IKeys';
+import config from '../../Config';
 interface DropdownProps {
   //keys: Keys.Key[];
   onSelect: (value: string) => void;
@@ -9,7 +10,7 @@ interface DropdownProps {
 //const Label = ({ id,caption,text}: LabelProps) => (
 
 const Dropdown = ({ onSelect }: DropdownProps) => {
-  const keys = DataLayer.GetKeys();
+  const response : useApi.ApiResult<IKey> = useApi.default<IKey>(config.apiUrl + '/Keys');
   const [selectedOption, setSelectedOption] = useState<string>('');
 
   const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -21,7 +22,7 @@ const Dropdown = ({ onSelect }: DropdownProps) => {
   return (
     <select id="Key-dropdown" value={selectedOption} onChange={handleOptionChange}>
       <option value="">Select an option</option>
-      {keys.map((key, index) => (
+      {response.data.map((key, index) => (
         <option key={index} value={key.KeyNotes}>
           {key.KeyNotes}
         </option>
