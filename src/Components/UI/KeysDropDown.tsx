991994f76ec09b243a69c78e6f8ebd8b0006aee1
Chapatta <49091220+Chapatta@users.noreply.github.com>
2024-05-14 20:48:32 +0100

    converted keys to promise

--- src/Components/UI/KeysDropDown.tsx
+++ src/Components/UI/KeysDropDown.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import * as useApi from '../../Services/API/APITool'; 
+import React, { useState , useEffect} from 'react';
+import * as useApi from '../../Services/API/APIPromise'; 
 import IKey from '../../DataLayer/Interfaces/IKeys';
 import config from '../../Config';
 interface DropdownProps {
@@ -10,7 +10,6 @@ interface DropdownProps {
 //const Label = ({ id,caption,text}: LabelProps) => (
 
 const Dropdown = (props: DropdownProps) => {
-  const response : useApi.ApiResult<IKey> = useApi.default<IKey>(config.apiUrl + '/Keys');
   const [selectedOption, setSelectedOption] = useState<string>('');
 
   const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -19,10 +18,32 @@ const Dropdown = (props: DropdownProps) => {
     props.onSelect(selectedValue);
   };
 
+  const [keys, setKeys] = useState<IKey[]>([]);
+
+  useEffect(() => {
+    // Fetch posts when component mounts
+    const client = new useApi.HTTPClient(config.apiUrl);
+    const fetchScaleTypes = async () => {
+      try {
+        const fetchedKeys = await client.get<IKey[]>('Keys');
+        setKeys(fetchedKeys);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchScaleTypes();
+
+    // Cleanup function (optional)
+    return () => {
+      // Cleanup logic (if any)
+    };
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
     <select id="Key-dropdown" value={selectedOption} onChange={handleOptionChange}>
       <option value="">Select an option</option>
-      {response.data.map((key, index) => (
+      {keys.map((key, index) => (
         <option key={index} value={key.KeyNotes}>
           {key.KeyNotes}
         </option>
